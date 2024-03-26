d71f9548b062184de6121dd8e20b5187cde8be75
Guillem <wimogas@gmail.com>
2024-03-26 14:23:39 +0100

    fix: refactor scss structure to improve customization

--- src/Grid/stories/Grid.stories.tsx
+++ src/Grid/stories/Grid.stories.tsx
@@ -5,10 +5,23 @@ export default {
 };
 
 const TemplateGrid: React.FC = (args) => (
-    <div className="bb-col-12">
-        <div className="bb-block"><div className="bb-bg-primary-500 bb-m-200">bb-col-12</div></div>
-        <div className="bb-block"><div className="bb-bg-primary-500 bb-m-200">bb-col-12</div></div>
+    <>
+    <div className="bb-neutral-400">.bb-cols-3 .bb-cols-sm-6 .bb-cols-md-12</div>
+    <div className="bb-cols-3 bb-cols-sm-6 bb-cols-md-12">
+        <div className="bb-block"><div className="bb-bg-primary-500 bb-m-200">bb-block</div></div>
+        <div className="bb-block"><div className="bb-bg-primary-500 bb-m-200">bb-block</div></div>
+        <div className="bb-block"><div className="bb-bg-primary-500 bb-m-200">bb-block</div></div>
+        <div className="bb-block"><div className="bb-bg-primary-500 bb-m-200">bb-block</div></div>
+        <div className="bb-block"><div className="bb-bg-primary-500 bb-m-200">bb-block</div></div>
+        <div className="bb-block"><div className="bb-bg-primary-500 bb-m-200">bb-block</div></div>
+        <div className="bb-block"><div className="bb-bg-primary-500 bb-m-200">bb-block</div></div>
+        <div className="bb-block"><div className="bb-bg-primary-500 bb-m-200">bb-block</div></div>
+        <div className="bb-block"><div className="bb-bg-primary-500 bb-m-200">bb-block</div></div>
+        <div className="bb-block"><div className="bb-bg-primary-500 bb-m-200">bb-block</div></div>
+        <div className="bb-block"><div className="bb-bg-primary-500 bb-m-200">bb-block</div></div>
+        <div className="bb-block"><div className="bb-bg-primary-500 bb-m-200">bb-block</div></div>
     </div>
+    </>
 );
 
 
